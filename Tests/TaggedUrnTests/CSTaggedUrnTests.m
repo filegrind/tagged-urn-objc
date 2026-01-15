8b9a339f26288905ddf27ec8d333bca55c09e64e
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-01-15 11:38:51 +0100

    Add utility methods for value quoting in CSTaggedUrn

--- Tests/TaggedUrnTests/CSTaggedUrnTests.m
+++ Tests/TaggedUrnTests/CSTaggedUrnTests.m
@@ -6,7 +6,7 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "CapNs.h"
+@import TaggedUrn;
 
 @interface CSTaggedUrnTests : XCTestCase
 @end
@@ -880,14 +880,14 @@
 
 - (void)testMatchingSemantics_Test7_FallbackPattern {
     // Test 7: Fallback pattern
-    // URN:     cap:op=generate_thumbnail;out=media:type=binary;v=1
-    // Request: cap:op=generate_thumbnail;out=media:type=binary;v=1;ext=wav
+    // URN:     cap:op=generate_thumbnail;out="media:type=binary;v=1"
+    // Request: cap:ext=wav;op=generate_thumbnail;out="media:type=binary;v=1"
     // Result:  MATCH (URN has implicit ext=*)
     NSError *error = nil;
-    CSTaggedUrn *urn = [CSTaggedUrn fromString:@"cap:op=generate_thumbnail;out=media:type=binary;v=1" error:&error];
+    CSTaggedUrn *urn = [CSTaggedUrn fromString:@"cap:op=generate_thumbnail;out=\"media:type=binary;v=1\"" error:&error];
     XCTAssertNotNil(urn);
 
-    CSTaggedUrn *request = [CSTaggedUrn fromString:@"cap:op=generate_thumbnail;out=media:type=binary;v=1;ext=wav" error:&error];
+    CSTaggedUrn *request = [CSTaggedUrn fromString:@"cap:ext=wav;op=generate_thumbnail;out=\"media:type=binary;v=1\"" error:&error];
     XCTAssertNotNil(request);
 
     BOOL matches = [urn matches:request error:&error];
